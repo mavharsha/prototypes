95ef22a8f73d8eafdb24a0d9b0a20ef6b0ca4464
mavharsha <sreeharsha.mamilla@gmail.com>
2026-04-21 23:58:30 -0500

    test(artemis-eh): shared Testcontainers harness and happy-path smoke test
    Deviations from plan:
    - Wait.forLogMessage(".*Server is now live.*") never matched; image emits
      "Server is now active". Swapped to Wait.forListeningPort() per gotcha #2.
    - Added @TestInstance(PER_CLASS) to test class because Micronaut's JUnit5
      extension only applies TestPropertyProvider when an instance exists at
      beforeAll time.
    - Added commons-codec 1.15 test dep because Testcontainers 1.20.4 references
      org.apache.commons.codec.Charsets, which newer commons-codec versions
      removed.

---
+++ events/artemis-broker-exception-handling/producer/src/test/java/com/mavharsha/events/artemis/producer/ArtemisTestResource.java
@@ -0,0 +1,38 @@
+package com.mavharsha.events.artemis.producer;
+
+import io.micronaut.test.support.TestPropertyProvider;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.MountableFile;
+
+import java.util.Map;
+
+public abstract class ArtemisTestResource implements TestPropertyProvider {
+
+    private static final GenericContainer<?> ARTEMIS;
+
+    static {
+        ARTEMIS = new GenericContainer<>("apache/activemq-artemis:2.33.0")
+                .withExposedPorts(61616, 8161)
+                .withEnv("ARTEMIS_USER", "artemis")
+                .withEnv("ARTEMIS_PASSWORD", "artemis")
+                .withEnv("ANONYMOUS_LOGIN", "false")
+                .withCopyFileToContainer(
+                        MountableFile.forHostPath("../infra/artemis/broker.xml"),
+                        "/var/lib/artemis-broker/etc/broker.xml")
+                .waitingFor(Wait.forListeningPort());
+        ARTEMIS.start();
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        String url = "tcp://" + ARTEMIS.getHost() + ":" + ARTEMIS.getMappedPort(61616);
+        return Map.of(
+                "micronaut.jms.activemq.artemis.enabled", "true",
+                "micronaut.jms.activemq.artemis.connection-string", url,
+                "micronaut.jms.activemq.artemis.username", "artemis",
+                "micronaut.jms.activemq.artemis.password", "artemis",
+                "emitter.enabled", "false"
+        );
+    }
+}
